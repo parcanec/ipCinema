2bc24c2af5f76254c7ab329bffb5b6d117dd2742
parcanec <igarek200706@gmail.com>
2023-03-02 17:17:16 +0200

    pagination - ok, typescript - bad

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -2,15 +2,21 @@ import {FC} from "react";
 import Item from "./item";
 import Pagination from "./pagination";
 import {items} from "../data";
+import { useState } from "react";
 
 const Dashboard: FC = (props) => {
+  const [page, setPage] = useState<number>(1)
+  const itemsPerPage = 9
+  const currentItems = items.slice((page-1)*itemsPerPage, page*itemsPerPage)
+  const pageCount = Math.ceil(items.length/itemsPerPage)
+
   return (
     <div>
         <div className="flex flex-wrap gap-6">
-          {items.map(item=><Item item={item} key={item.id}/>)}
+          {currentItems.map(item=><Item item={item} key={item.id}/>)}
         </div>
         <div >
-            <Pagination/>
+          <Pagination pageCount={pageCount} page={page} setPage={setPage}/>
         </div>
     </div>
     )
--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -4,11 +4,15 @@ const Pagination: FC = (props) => {
     return (
         <div>
             <div className={'flex gap-4 mb-4'}>
-                <button className={'bg-violet-300 rounded-full py-2 px-4 text-white mt-12'}>Назад</button>
-                <button className={'bg-violet-600 rounded-full py-2 px-4 text-white mt-12'}>Вперед</button>
+                <button className={'bg-violet-300 rounded-full py-2 px-4 text-white mt-12'}
+                disabled={props.page===1} 
+                onClick={() => props.setPage(props.page-1)} >Назад</button>
+                <button className={'bg-violet-600 rounded-full py-2 px-4 text-white mt-12'}
+                disabled={props.page===props.pageCount} 
+                onClick={() => props.setPage(props.page+1)} >Вперед</button>
             </div>
             <div>
-                1 из 1000
+                {props.page} из {props.pageCount}
             </div>
         </div>
     )

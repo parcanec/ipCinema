98547d50dc16152bdae19ef13db6c167d295b060
parcanec <igarek200706@gmail.com>
2023-03-03 17:39:09 +0200

    needed redux for filters

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import { Provider } from "react-redux";
 import Home from "./components/home";
 import LoginPage from "./components/login";
-
+//mport store from "./store/store.js"
 import Header from "./components/header"
 import {Routes, Route } from "react-router-dom"
 

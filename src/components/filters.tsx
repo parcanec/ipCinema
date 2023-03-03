acf94040d69f55bf7ebfb003d3b3289a5f3f8510
sgu6ena <sgu6ena@gmail.com>
2023-03-03 14:21:44 +0200

    сорт

--- src/components/filters.tsx
+++ src/components/filters.tsx
@@ -1,47 +1,70 @@
 import React, {FC, useEffect, useState} from 'react';
 import {genres} from '../data';
 
+export interface IFitler {
+  value: number
+  label: string
+}
+
+const YEARS: number[] = [2020, 2019, 2018, 2017]
+const FILTERS: IFitler[] = [
+  {
+    value: 0, label: "Популярные по убыванию"
+  }, {
+    value: 1, label: "Популярные по возрастанию"
+  }, {
+    value: 2, label: "Рейтинг по убыванию"
+  }, {
+    value: 3, label: "Рейтинг по возрастанию"
+  }
+]
 const Filters = () => {
-  const [year, setYear] = useState<number>(2020)
-  console.log(`${year} is =>>> ${typeof year}`)
-  const [sort, setSort] = useState<string>('Популярные по убыванию')
-  console.log(`${sort} is =>>> ${typeof sort}`)
-  
-  useEffect(()=> {}, [])
+  const [year, setYear] = useState<number>(YEARS[0])
+  const [sort, setSort] = useState<IFitler>(FILTERS[0])
 
   return (
     <div className='flex flex-col gap-4'>
       <div className="font-bold">Фильтры:</div>
-      <button className="bg-black text-white font-bold rounded py-2 px-4 items-center w-[100%] m" onClick={() =>{
-        setYear(2020)
-        setSort('Популярные по убыванию')
+      <button className="bg-black text-white font-bold rounded py-2 px-4 items-center w-[100%] m" onClick={() => {
+        setYear(YEARS[0])
+        setSort(FILTERS[0])
       }
-      }>Сбросить все фильтры</button>
+      }>Сбросить все фильтры
+      </button>
       <div className="font-light">Сортировать по:</div>
       <select className="w-[100%] text-lg p-2 pe-4 rounded-xl" onChange={
-        e => {setSort(String(e.target.value))}}>
-        <option value={'Популярные по убыванию'}>Популярные по убыванию</option>
-        <option value={'Популярные по возрастанию'}>Популярные по возрастанию</option>
-        <option value={'Рейтинг по убыванию'}>Рейтинг по убыванию</option>
-        <option value={'Рейтинг по возрастанию'}>Рейтинг по возрастанию</option>
+        e => setSort(FILTERS?.find(filter => filter.value === Number(e.target.value)) || FILTERS[0])
+      }>
+        {FILTERS.map(filter => (
+          <option
+            key={filter.value}
+            value={filter.value}
+            selected={filter.value === sort.value}>
+            {filter.label}
+          </option>
+        ))}
       </select>
       <div className="font-light">Год релиза:</div>
       <select className="w-[100%] text-lg p-2 pe-4 rounded-xl" onChange={
-        e => {setYear(Number(e.target.value))}}>
-        <option value={2020}>2020</option>
-        <option value={2019}>2019</option>
-        <option value={2018}>2018</option>
-        <option value={2017}>2017</option>
+        e => setYear(YEARS?.find(item => item === Number(e.target.value)) || YEARS[0])
+      }>
+        {YEARS.map(item => (
+          <option
+            key={item}
+            value={item}
+            selected={item === year}>
+            {item}
+          </option>
+        ))}
       </select>
       <div className={'flex flex-col gap-2'}>
         <div className="font-light mb-1">Жанры:</div>
         {
-          genres.map(genre=>(
+          genres.map(genre => (
             <label key={genre.id}><input type="checkbox"/> {genre.name}</label>
           ))
         }
       </div>
-
     </div>
   );
 };
--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,26 +1,31 @@
 import {FC} from "react";
 
 interface IPagination {
-    page: number
-    pageCount: number
-    setPage: (arg0:number)=>void}
+  page: number
+  pageCount: number
+  setPage: (arg0: number) => void
+}
 
 const Pagination: FC<IPagination> = (props) => {
-    return (
-        <div>
-            <div className={'flex gap-4 mb-4'}>
-                <button className={`${props.page===1?'bg-violet-300':'bg-violet-600'} rounded-full py-2 px-4 text-white mt-12`}
-                disabled={props.page===1}
-                onClick={() => props.setPage(props.page-1)} >Назад</button>
-                <button className={`${props.page===props.pageCount?'bg-violet-300':'bg-violet-600'} rounded-full py-2 px-4 text-white mt-12`}
-                disabled={props.page===props.pageCount} 
-                onClick={() => props.setPage(props.page+1)} >Вперед</button>
-            </div>
-            <div>
-                {props.page} из {props.pageCount}
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className={'flex gap-4 mb-4'}>
+        <button
+          className={`${props.page === 1 ? 'bg-violet-300' : 'bg-violet-600'} rounded-full py-2 px-4 text-white mt-12`}
+          disabled={props.page === 1}
+          onClick={() => props.setPage(props.page - 1)}>Назад
+        </button>
+        <button
+          className={`${props.page === props.pageCount ? 'bg-violet-300' : 'bg-violet-600'} rounded-full py-2 px-4 text-white mt-12`}
+          disabled={props.page === props.pageCount}
+          onClick={() => props.setPage(props.page + 1)}>Вперед
+        </button>
+      </div>
+      <div>
+        {props.page} из {props.pageCount}
+      </div>
+    </div>
+  )
 }
 
 export default Pagination

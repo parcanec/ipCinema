28c8f6c1ec08f0b46f3249b4099a82fa64daee9b
parcanec <igarek200706@gmail.com>
2023-03-03 09:20:26 +0200

    pagination - OK

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -9,10 +9,10 @@ const Pagination: FC<IPagination> = (props) => {
     return (
         <div>
             <div className={'flex gap-4 mb-4'}>
-                <button className={'bg-violet-300 rounded-full py-2 px-4 text-white mt-12'}
-                disabled={props.page===1} 
+                <button className={`${props.page===1?'bg-violet-300':'bg-violet-600'} rounded-full py-2 px-4 text-white mt-12`}
+                disabled={props.page===1}
                 onClick={() => props.setPage(props.page-1)} >Назад</button>
-                <button className={'bg-violet-600 rounded-full py-2 px-4 text-white mt-12'}
+                <button className={`${props.page===props.pageCount?'bg-violet-300':'bg-violet-600'} rounded-full py-2 px-4 text-white mt-12`}
                 disabled={props.page===props.pageCount} 
                 onClick={() => props.setPage(props.page+1)} >Вперед</button>
             </div>
